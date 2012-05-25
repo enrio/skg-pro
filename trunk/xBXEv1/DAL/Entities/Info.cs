3b6bb5f06dcc8bc69dddefbb2d7209be9fceb546
nvt87x@gmail.com <nvt87x@gmail.com@767163a7-b11b-719d-c8f8-a4db396ef0e7>
2012-05-25 14:04:22 +0000

    Thông tin dữ liệu cơ bản (thay đổi mới)

--- trunk/xBXEv1/DAL/Entities/Info.cs
+++ trunk/xBXEv1/DAL/Entities/Info.cs
@@ -5,18 +5,22 @@ using System.Text;
 
 namespace DAL.Entities
 {
+    using System.ComponentModel.DataAnnotations;
+
     /// <summary>
-    /// Thông tin dữ liệu
+    /// Thông tin dữ liệu cơ bản
     /// </summary>
     public abstract class Info
     {
+        [Key, Column(Order = 0)]
+        public Guid Id { set; get; }
+
         public string Code { set; get; }
+        public string Descript { set; get; }
+
         public int Order { set; get; }
         public bool Show { set; get; }
 
-        public Info()
-        {
-            Show = true;
-        }
+        public Info() { Show = true; }
     }
 }

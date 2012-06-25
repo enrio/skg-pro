d8ba55e34a7febf4cb6d3ebeb710a1057e154bce
sxxuxxs@gmail.com <sxxuxxs@gmail.com@767163a7-b11b-719d-c8f8-a4db396ef0e7>
2012-06-25 13:07:24 +0000

    SKG.BLL OK!

---
+++ trunk/xSKGv1/SKG.BLL/Pol_RoleRightBLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BLL
+{
+    /// <summary>
+    /// Truy cập cơ sở dữ liệu bảng Pol_RoleRight: nhóm quyền - phân quyền trên từng form theo nhóm quyền.
+    /// </summary>
+    public sealed class Pol_RoleRightBLL : DAL.Pol_RoleRightDAL
+    {
+    }
+}

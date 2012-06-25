c3c5798eb050744560a1aaacafa4ba68d549fe79
sxxuxxs <sxxuxxs@gmail.com>
2012-06-25 13:07:24 +0000

    SKG.BLL OK!

---
+++ xSKGv1/SKG.BLL/Pol_UserRoleBLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BLL
+{
+    /// <summary>
+    /// Truy cập cơ sở dữ liệu bảng Pol_UserRole - người dùng thuộc nhóm quyền.
+    /// </summary>
+    public sealed class Pol_UserRoleBLL : DAL.Pol_UserRoleDAL
+    {
+    }
+}

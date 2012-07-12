907bd024ffb808be3196aead98c87cf736237bc6
sxxuxxs@gmail.com <sxxuxxs@gmail.com@767163a7-b11b-719d-c8f8-a4db396ef0e7>
2012-07-12 00:56:28 +0000

    SKG

---
+++ trunk/mco/SKG/Plugin/IHost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SKG.Plugin
+{
+    /// <summary>
+    /// Interface of host
+    /// </summary>
+    public interface IHost
+    {
+        /// <summary>
+        /// Feed back
+        /// </summary>
+        /// <param name="feedBack">Feed back</param>
+        /// <param name="plugin">Plugin</param>
+        void FeedBack(string feedBack, IPlugin plugin);
+
+        /// <summary>
+        /// Register plugin
+        /// </summary>
+        /// <param name="plugin">Plugin</param>
+        /// <returns></returns>
+        bool Register(IPlugin plugin);
+
+        /// <summary>
+        /// Load all plugins
+        /// </summary>
+        void LoadPlugins();
+    }
+}

db1b9dbc067f1256dcbfaedac5f7c2dcfe7f1710
sxxuxxs@gmail.com <sxxuxxs@gmail.com@767163a7-b11b-719d-c8f8-a4db396ef0e7>
2012-07-12 00:45:34 +0000

    Standard

---
+++ trunk/mco/SKG.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SKG.Client
+{
+    using DevExpress.Skins;
+    using DevExpress.LookAndFeel;
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            SkinManager.EnableFormSkins();
+            DevExpress.UserSkins.BonusSkins.Register();
+            UserLookAndFeel.Default.SetSkinStyle("DevExpress Style");
+
+#if DEBUG
+            //BLL.Sample.CreateData(false);
+#endif
+            var frm = Properties.Settings.Default.IsDevExpress ? (Form)new DXF.FrmDemo() : (Form)new MSF.FrmDemo();
+            Application.Run(frm);
+        }
+    }
+}

586fd73f38e765a5ba509e5342513be1062e8bf6
sxxuxxs <sxxuxxs@gmail.com>
2012-07-21 07:36:29 +0000

    SKG

---
+++ xSKGv1/SKG/DAL/Entities/Pol_Action.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SKG.DAL.Entities
+{
+    /// <summary>
+    /// Chính sách - Danh sách tên các hành động: Thêm, Sửa, Xoá, In ấn, Truy cập, Tất cả, Không có, Mặc định
+    /// </summary>
+    public class Pol_Action : ZBase
+    {
+        /// <summary>
+        /// Tên hành động
+        /// </summary>
+        public string Name { set; get; }
+    }
+}

6a5c037405abe2d00de64dc020ff8b172bdb2a84
sxxuxxs <sxxuxxs@gmail.com>
2012-11-02 02:45:21 +0000

    Sửa lỗi chết người: Xoa DB neu cau truc DB thay doi

--- xSKGv1/SKG/DAL/BaseDAL.cs
+++ xSKGv1/SKG/DAL/BaseDAL.cs
@@ -41,8 +41,8 @@ namespace SKG.DAL
         /// </summary>
         public BaseDAL()
         {
-            Database.SetInitializer<Context>(new DropCreateDatabaseIfModelChanges<Context>());
-            //Database.SetInitializer<Context>(new CreateDatabaseIfNotExists<Context>());
+            //Database.SetInitializer<Context>(new DropCreateDatabaseIfModelChanges<Context>());
+            Database.SetInitializer<Context>(new CreateDatabaseIfNotExists<Context>());
         }
 
         /// <summary>

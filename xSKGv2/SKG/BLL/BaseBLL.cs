8bb87a0b1e649c32a196ce8c12160f0b0459b5fd
nvt87x <nvt87x@gmail.com>
2012-12-19 17:06:14 +0000

    SKG

---
+++ xSKGv2/SKG/BLL/BaseBLL.cs
@@ -0,0 +1,100 @@
+﻿#region Information
+/*
+ * Author: Zng Tfy
+ * Email: nvt87x@gmail.com
+ * Phone: +84 1645 515 010
+ * ---------------------------
+ * Create: 24/07/2012 21:33
+ * Update: 26/07/2012 14:22
+ * Status: OK
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SKG.BLL
+{
+    using SKG.Datax;
+
+    /// <summary>
+    /// Data sample, all of flow processing
+    /// </summary>
+    public class BaseBLL
+    {
+        #region Properties access database
+        /// <summary>
+        /// Policy - Pol_Dictionary accessing
+        /// </summary>
+        public Pol_DictionaryBLL Pol_Dictionary { set; get; }
+
+        /// <summary>
+        /// Policy - Pol_User accessing
+        /// </summary>
+        public Pol_UserBLL Pol_User { set; get; }
+
+        /// <summary>
+        /// Policy - Pol_UserRight accessing
+        /// </summary>
+        public Pol_UserRightBLL Pol_UserRight { set; get; }
+
+        /// <summary>
+        /// Policy - Pol_UserRole accessing
+        /// </summary>
+        public Pol_UserRoleBLL Pol_UserRole { set; get; }
+
+        /// <summary>
+        /// Policy - Pol_RoleRight accessing
+        /// </summary>
+        public Pol_RoleRightBLL Pol_RoleRight { set; get; }
+
+        /// <summary>
+        /// Truy cập cơ sở dữ liệu bảng Tra_Kind: danh mục loại xe.
+        /// </summary>
+        public Tra_TariffBLL Tra_Tariff { set; get; }
+
+        /// <summary>
+        /// Truy cập cơ sở dữ liệu bảng Tra_Vehicle: danh sách xe cộ.
+        /// </summary>
+        public Tra_VehicleBLL Tra_Vehicle { set; get; }
+
+        /// <summary>
+        /// Truy cập cơ sở dữ liệu bảng Tra_Detail: chi tiết xe ra vào, bến.
+        /// </summary>
+        public Tra_DetailBLL Tra_Detail { set; get; }
+        #endregion
+
+        /// <summary>
+        /// Constructor access database
+        /// </summary>
+        public BaseBLL()
+        {
+            Pol_Dictionary = new Pol_DictionaryBLL();
+            Pol_User = new Pol_UserBLL();
+            Pol_UserRight = new Pol_UserRightBLL();
+            Pol_UserRole = new Pol_UserRoleBLL();
+            Pol_RoleRight = new Pol_RoleRightBLL();
+            Tra_Tariff = new Tra_TariffBLL();
+            Tra_Vehicle = new Tra_VehicleBLL();
+            Tra_Detail = new Tra_DetailBLL();
+        }
+
+        #region Static methods
+        /// <summary>
+        /// All of flow processing
+        /// </summary>
+        public static BaseBLL _bll = new BaseBLL();
+
+        /// <summary>
+        /// Check database exists
+        /// </summary>
+        /// <returns></returns>
+        public static bool CheckDb()
+        {
+            var svr = new SqlServer(Global.Connection.ConnectionString);
+            return svr.CheckDbExists(Global.Connection.Database);
+        }
+        #endregion
+    }
+}
---
+++ xSKGv2/SKG/DAL/Entities/Pol_User.cs
@@ -0,0 +1,104 @@
+﻿#region Information
+/*
+ * Author: Zng Tfy
+ * Email: nvt87x@gmail.com
+ * Phone: +84 1645 515 010
+ * ---------------------------
+ * Create: 09/08/2013 20:32
+ * Update: 09/08/2013 20:32
+ * Status: OK
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SKG.DAL.Entities
+{
+    /// <summary>
+    /// Policy - User's information
+    /// </summary>
+    public class Pol_User : Zinfors
+    {
+        #region Foreign key
+        /// <summary>
+        /// User's list has permission
+        /// </summary>
+        public virtual ICollection<Pol_UserRight> UserRights { get; set; }
+
+        /// <summary>
+        /// List of user belong group
+        /// </summary>
+        public virtual ICollection<Pol_UserRole> UserRoles { get; set; }
+
+        /// <summary>
+        /// List of user's language choice
+        /// </summary>
+        public virtual ICollection<Pol_Selection> Selections { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Account login
+        /// </summary>
+        public string Acc { set; get; }
+
+        /// <summary>
+        /// Password login
+        /// </summary>
+        public string Pass { set; get; }
+
+        /// <summary>
+        /// User's full name
+        /// </summary>
+        public string Name { set; get; }
+
+        /// <summary>
+        /// User's birthday
+        /// </summary>
+        public DateTime Birth { set; get; }
+
+        /// <summary>
+        /// User's address
+        /// </summary>
+        public string Address { set; get; }
+
+        /// <summary>
+        /// User's phone
+        /// </summary>
+        public string Phone { set; get; }
+
+        /// <summary>
+        /// Check user's role
+        /// </summary>
+        /// <param name="roleCode">Role of code</param>
+        /// <returns></returns>
+        public bool CheckRole(string roleCode)
+        {
+            try
+            {
+                var role = UserRoles.Where(k => k.Role.Code == roleCode);
+                return (role.Count() > 0);
+            }
+            catch { return false; }
+        }
+
+        /// <summary>
+        /// Check role administrator
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckAdmin()
+        {
+            return CheckRole("QT") || (Acc.ToLower() == "admin");
+        }
+
+        /// <summary>
+        /// Check role operator teams
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckOperator()
+        {
+            return CheckRole("QL");
+        }
+    }
+}

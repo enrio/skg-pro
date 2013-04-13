0c0888cb3b37bbe8256ed44ae861680686f8e813
nvt87x <nvt87x@gmail.com>
2013-04-13 15:19:00 +0000

    Group by UserRight

--- xSKGv1/SKG/BLL/Pol_UserRightBLL.cs
+++ xSKGv1/SKG/BLL/Pol_UserRightBLL.cs
@@ -11,8 +11,9 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace SKG.BLL
 {
@@ -21,5 +22,44 @@ namespace SKG.BLL
     /// </summary>
     public sealed class Pol_UserRightBLL : DAL.Pol_UserRightDAL
     {
+        public new DataTable Select(object obj = null, int skip = 0, int take = 0)
+        {
+            var tb = base.Select(obj, skip, take);
+            var str = String.Format("RightId = '{0}'", Guid.Empty);
+
+            var user = tb.Select(str, "");
+            for (int i = 0; i < user.Count(); i++)
+            {
+                var id = user[i]["Id"];
+                str = "xParentID = '{0}' and ParentID = '{0}' and RightId <> '{1}'";
+                str = String.Format(str, id, Guid.Empty);
+
+                var l1 = tb.Select(str, "");
+                foreach (DataRow r1 in l1)
+                {
+                    var tmp = r1["Id"];
+                    r1["Id"] = r1["xId"];
+                    str = String.Format("xParentID = '{0}' and ParentID = '{1}'", id, tmp);
+
+                    var l2 = tb.Select(str, "");
+                    foreach (DataRow r2 in l2)
+                    {
+                        tmp = r2["Id"];
+                        r2["ParentID"] = r1["Id"];
+                        r2["Id"] = r2["xId"];
+                        str = String.Format("xParentID = '{0}' and ParentID = '{1}'", id, tmp);
+
+                        var l3 = tb.Select(str, "");
+                        foreach (DataRow r3 in l3)
+                        {
+                            r3["ParentID"] = r2["Id"];
+                            r3["Id"] = r3["xId"];
+                        }
+                    }
+                }
+            }
+
+            return tb;
+        }
     }
 }
